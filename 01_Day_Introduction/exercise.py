a82dc56c72ecf3a1f587dc64c6d3552e1cd68739
ctneal91 <ct@quantlink.ai>
2026-02-19 13:49:38 -0600

    Complete Day 01 exercises

---
+++ 01_Day_Introduction/exercise.py
@@ -0,0 +1,21 @@
+import math
+
+# 1. Print your age to the console.
+print("My age:")
+print(33)
+
+# 2. Calculate and print the number of days, weeks, and months in 27 years. Don’t worry about leap years!
+print("Months in 27 years:")
+print(27 * 12)
+print("Weeks in 27 years:")
+print(27 * 52)
+print("Days in 27 years:")
+print(27 * 365 + 6)
+
+# 3. Calculate and print the area of a circle with a radius of 5 units. You can be as accurate as you like with the value of pi.
+print("Area of a circle (pi = 3.14):")
+print(5**2 * 3.14)
+print("Area of a circle (pi = 22/7):")
+print(5**2 * 22/7)
+print("Area of a circle (pi = math.pi):")
+print(5**2 * math.pi)
